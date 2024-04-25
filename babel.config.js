872f41ef846431bbfcf50ab1a466f2c131c7b35f
Lai Quang Phat <77201894+quangphatt@users.noreply.github.com>
2024-04-26 00:27:31 +0700

    Install module

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,28 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        alias: {
+          '@assets': './src/assets',
+          '@navigation': './src/navigation',
+          '@screens': './src/screens',
+        }
+      }
+    ],
+    [
+      'module:react-native-dotenv',
+      {
+        envName: 'APP_ENV',
+        moduleName: '@env',
+        path: '.env',
+        safe: false,
+        allowUndefined: true,
+        verbose: false
+      }
+    ],
+    'react-native-reanimated/plugin'
+  ],
 };
