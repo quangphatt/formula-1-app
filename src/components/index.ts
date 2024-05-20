8fa60b5ba7cdef01df02fc9d31f55d807b177a68
Lai Quang Phat <77201894+quangphatt@users.noreply.github.com>
2024-05-20 23:19:24 +0700

    SearchTextInput

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,4 +2,5 @@ export * from './common/Button';
 export * from './Header';
 export * from './common/Icon';
 export * from './common/Image';
+export * from './common/SearchTextInput';
 export * from './common/Text';
